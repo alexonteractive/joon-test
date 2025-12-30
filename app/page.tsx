ec32325417087ecd1b90601c3eb49d61b00d5094
Alex <alex@onteractive.eu>
2025-12-31 01:57:04 +0700

    finish the first code gen today

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { ComponentExample } from "@/components/component-example";
+import React from "react";
+import { ComponentExample } from "../components/component-example";
 
 export default function Page() {
   return (
-    <div>
-      <h1>I am building something here</h1>
-      <p>This is vdery confusing</p>
+    <div className="container mx-auto px-4 py-8 max-w-6xl">
+      <h1 className="text-2xl font-bold">I am building something here</h1>
+      <p>This is very confusing, but it's working so should be ok lol</p>
       <ComponentExample />
     </div>
   );
