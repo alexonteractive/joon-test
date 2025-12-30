1125fc431e0536f357fb503ca9d1f85d2fb2fc85
Alex <alex@onteractive.eu>
2025-12-31 01:33:03 +0700

    started a nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { ComponentExample } from "@/components/component-example";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>I am building something here</h1>
+      <p>This is vdery confusing</p>
+      <ComponentExample />
+    </div>
+  );
+}
